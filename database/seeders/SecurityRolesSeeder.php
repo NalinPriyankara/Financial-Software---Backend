b7c494387425f058cd32dbb7a5c60af0629a4016
NalinPriyankara <nalinpriyankara033@gmail.com>
2026-02-20 16:40:04 +0530

    role update

--- database/seeders/SecurityRolesSeeder.php
+++ database/seeders/SecurityRolesSeeder.php
@@ -17,8 +17,8 @@ class SecurityRolesSeeder extends Seeder
             [
                 'role' => 'Admin',
                 'description' => 'Administrator role with full access',
-                'sections' => "1000;1100;1200",
-                'areas' => "1101;1102;1103;1201;1202;1203",
+                'sections' => "1000;1100;1200;1300;1400;1500;1600;1700;1800;1900;2000;2100;2200",
+                'areas' => "1101;1102;1103;1201;1202;1301;1302;1303;1401;1402;1403;1501;1502;1503;1601;1602;1603;1701;1702;1703;1801;1802;1803;1901;1902;2001;2002;2101;2201;2202",
                 'inactive' => false,
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -26,8 +26,8 @@ class SecurityRolesSeeder extends Seeder
             [
                 'role' => 'User',
                 'description' => 'Regular user role with limited access',
-                'sections' => "1000;1100",
-                'areas' => "1101;1102;1103",
+                'sections' => "1000;1100;1300;1400;1500;1600;1700;1800;1900;2000;2100",
+                'areas' => "1101;1102;1103;1301;1302;1303;1401;1402;1403;1501;1502;1503;1601;1602;1603;1701;1702;1703;1801;1802;1803;1901;1902;2001;2002;2101",
                 'inactive' => false,
                 'created_at' => now(),
                 'updated_at' => now(),
